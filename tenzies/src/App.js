69d1d1b265f7448efbb29f210aec1dfb58bd2275
farukh <farukh022198@gmail.com>
2023-07-11 20:22:04 -0400

    updated hold dice and new game

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -2,30 +2,59 @@
 import React from "react"
 import Die from "./Die.js"
 import { nanoid } from 'nanoid'
+import Confetti from "react-confetti"
+
 export default function App() {
 
   const [dices, setDices]  = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
 
+  React.useEffect(()=>{
+    const allHeld = dices.every(die => die.isHeld)
+    const firstValue = dices[0].value
+    const allSameValue = dices.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+        setTenzies(true)
+        console.log("You won!")
+    }
+  }, dices);
+
+  function generateDice(){
+    return { 
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
   
   function allNewDice(){
     
     let dice = [];
-    let max = 6;
-    let min = 1; 
     for(let i=0; i<10; i++){
-      let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-      dice[i] = {value: randomNum, 
-        isHeld: false,
-        id: nanoid()
-      } 
-    }
+      dice.push(generateDice())
+    }  
     return dice; 
   }
 
-  function generateDices(){
-
-    setDices(() => allNewDice())
-  }
+  function rollDice() {
+    if(!tenzies){
+      setDices(prevDice => {
+          let newDice = []; 
+          for(let i=0; i<prevDice.length; i++){
+              let currentDice = prevDice[i]; 
+              if(currentDice.isHeld === false){
+                  newDice.push(generateDice())
+              }else{
+                  newDice.push(currentDice) 
+              } 
+          }
+          return newDice; 
+      })
+    }else{
+      setTenzies(false);
+      setDices(allNewDice()); 
+    }  
+}
 
   function holdDice(id){
     setDices(oldDice => oldDice.map(die => {
@@ -42,13 +71,18 @@ export default function App() {
     holdDice = {() => holdDice(dice.id)}
   />)
 
+  const rollButton = tenzies? "New Game": "Roll" 
+
 
     return (
         <main>
+          {tenzies && <Confetti />}
+          <h1 className="title">Tenzies</h1>
+          <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
           <div className="dies-container">
             {diecesElements}
           </div>
-          <button className="roll-dice" onClick={generateDices}>Roll</button>
+          <button className="roll-dice" onClick={rollDice}>{rollButton}</button>
         </main>
     )
 }
